37c411f8d97c59f04063e747bfad64adb9998e2d
Sal Ferrarello <sal@ironcodestudio.com>
2021-04-15 14:30:22 -0400

    Add Higher Order Component (HOC) withMuppets()
    This HOC provides the "muppets" prop to the wrapped component.
    
    See #1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import MuppetList from './components/MuppetList';
+import withMuppets from './components/withMuppets';
 
 function App() {
+  const MuppetListWithMuppets = withMuppets(MuppetList);
   return (
     <div className="App">
-        <MuppetList />
+      <MuppetListWithMuppets />
     </div>
   );
 }
