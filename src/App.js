597178beb66df11ab4fb03b53343f5e1d4dddf32
Sal Ferrarello <sal@ironcodestudio.com>
2021-04-15 14:29:23 -0400

    Remove MuppetsProvider
    Remove MuppetsProvider, this breaks our App however we will fix it in a
    moment with the addition of the Higher Order Component (HOC) withMuppets()
    
    See #1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import MuppetsProvider from './components/MuppetsProvider';
 import MuppetList from './components/MuppetList';
 
 function App() {
   return (
     <div className="App">
-      <MuppetsProvider>
         <MuppetList />
-      </MuppetsProvider>
     </div>
   );
 }
